d572d564fa0e857fa44526e7dd0edfd17990115c
Sherry McCarty <sherrydmccarty@gmail.com>
2020-05-11 15:31:30 -0400

    added CSS files

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import List from './List.js'
 function App() {
   return (
     <main className='App'>
-    {STORE.lists.map(list => <List header={list.header} key={list.id} cards={list.cardIds.map(cardId => STORE.allCards[cardId])}></List>)}
-
+      <header className="App-header">
+        <h1>Trelloyes!</h1>
+      </header>
+      <div className="App-list">
+        {STORE.lists.map(list => <List header={list.header} key={list.id} cards={list.cardIds.map(cardId => STORE.allCards[cardId])}></List>)}
+      </div>
     </main>
   );
 }
