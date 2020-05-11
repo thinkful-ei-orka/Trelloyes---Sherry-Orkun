969f8404835f58cb951a0dae930f9c65f3bb31a0
Orkun Ozekin <orkunozekin@thinkful.com>
2020-05-11 13:59:13 -0500

    first steps

---
+++ src/List.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './Card.js'
+
+function List(props) {
+    return (
+    
+    <section className="List">
+        <header className="List-header">
+          <h2>{props.header}</h2>
+          {props.cards.map(card => <Card title={card.title} content={card.content} key={card.id}/>)}
+        </header>  
+    </section>
+    )
+}
+
+export default List;
